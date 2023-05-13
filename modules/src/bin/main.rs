67ac114a49a2954d2f70838339b2a290d4afbde7
Christopher Repka <49252844+ctrepka@users.noreply.github.com>
2023-05-13 06:48:56 +0000

    renamed imports

--- modules/src/bin/main.rs
+++ modules/src/bin/main.rs
@@ -1,10 +1,10 @@
 //use lib modules from package binary
 
-//use restaurant::front_of_house::hosting;
-//use restaurant::eat_at_restaurant;
+//use modules::front_of_house::hosting;
+//use modules::eat_at_restaurant;
 
 //illustrate use of nested path import syntax
-use restaurant::{eat_at_restaurant, front_of_house::hosting};
+use modules::{eat_at_restaurant, front_of_house::hosting};
 
 fn main() {
     hosting::add_to_waitlist(String::from("Christopher"));
